38532c0f68c342e8f322eb772a3e918c517da0f7
Eclipsum <wozzswe@gmail.com>
2018-11-27 15:17:13 +0100

    Initial commit

---
+++ authlib/ESHA256.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace authlib
+{
+    public class ESHA256
+    {
+        private string _Hash;
+
+        public ESHA256(string Input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(Input));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                _Hash = builder.ToString();
+            }
+        }
+
+        public string Hash
+        {
+            get
+            {
+                return _Hash;
+            }
+        }
+    }
+}
