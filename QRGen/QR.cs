07367ee7b82fc326241de71e2e0d956b8475b943
VB <victor.beck123123@gmail.com>
2018-11-28 12:48:57 +0100

    Made QRGen offline

--- QRGen/QR.cs
+++ QRGen/QR.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using QRCoder;
 
 namespace QRGen
 {
@@ -12,33 +13,10 @@ namespace QRGen
     {
         public String Create(String content)
         {
-            StringBuilder _sb = new StringBuilder();
-            Byte[] _byte = this.GetImage("http://eclteam.cf/qr.php?content=" + content);
-            _sb.Append(Convert.ToBase64String(_byte, 0, _byte.Length));
-            return _sb.ToString();
-        }
-
-        private byte[] GetImage(string url)
-        {
-            Stream stream = null;
-            byte[] buf;
-            try
-            {
-                WebProxy myProxy = new WebProxy();
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
-                stream = response.GetResponseStream();
-                using (BinaryReader br = new BinaryReader(stream))
-                {
-                    int len = (int)(response.ContentLength);
-                    buf = br.ReadBytes(len);
-                    br.Close();
-                }
-                stream.Close();
-                response.Close();
-            }
-            catch {buf = null;}
-            return (buf);
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
+            Base64QRCode qrCode = new Base64QRCode(qrCodeData);
+            return qrCode.GetGraphic(20);
         }
     }
 }
---
+++ QRTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRGen;
+using System.IO;
+
+namespace QRTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            QR qrcode = new QR();
+            byte[] bD = Convert.FromBase64String(qrcode.Create("TokenGoesHere"));
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.StreamSource = new MemoryStream(bD);
+            bi.EndInit();
+            Image img = new Image();
+            img.Source = bi;
+            wpfImage.Source = bi;
+        }
+    }
+}
